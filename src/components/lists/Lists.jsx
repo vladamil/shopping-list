506333e0b615a42dd03831dd4824a5407d698b47
Vladimir Milinic <vladimir.milinic@gmail.com>
2023-10-29 14:35:29 +0100

    styled main page when there are no lists to show

--- src/components/lists/Lists.jsx
+++ src/components/lists/Lists.jsx
@@ -1,11 +1,26 @@
 import { useContext } from 'react';
 import ListPreview from './ListPreview';
 import ListContext from '../../context/ListContext';
+import logo from '../../img/logo.png';
 
 import styles from './Lists.module.css';
 
 function Lists() {
-   const { lists } = useContext(ListContext);
+   const { lists, displayForm } = useContext(ListContext);
+
+   if (!lists || lists.length === 0) {
+      return (
+         <div className={styles.empty}>
+            <h2>Welcome to your Shopping List</h2>
+            <p>No shopping lists yet!</p>
+            <img src={logo} alt="logo" />
+            <p className={styles.moto}>It is time to go shopping!!</p>
+            <p className={styles.link} onClick={() => displayForm()}>
+               Create new list
+            </p>
+         </div>
+      );
+   }
 
    return (
       <div className={styles['lists-container']}>
